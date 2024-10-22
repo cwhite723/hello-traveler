c6dccef11d8d8fbd9b6f7d67d0025be2570cb175
cwhite723 <c_white723@naver.com>
2024-10-23 06:42:29 +0900

    first commit

---
+++ src/main/java/com/hayan/hello_traveler/HelloTravelerApplication.java
@@ -0,0 +1,13 @@
+package com.hayan.hello_traveler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTravelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTravelerApplication.class, args);
+	}
+
+}
